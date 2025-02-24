59e81229b6f63ece4cbba3f0db9d365367f0d542
PHDinbeingLAZY <shanmukharap@gmail.com>
2025-02-24 23:26:01 +0400

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const chatbotFrame = document.getElementById('chatbot-frame');
+    chatbotFrame.src = "https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2025/02/24/07/20250224074711-UKW1SVXN.json";
+});
